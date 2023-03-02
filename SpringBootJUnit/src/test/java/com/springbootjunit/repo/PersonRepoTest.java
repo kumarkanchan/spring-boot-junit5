26a6b7ddd1be1bcf2ecc6f88cf90831f4319d4a3
Kanchan <info.kanchankumar@gmail.com>
2023-03-02 16:00:12 +0530

    service test

--- SpringBootJUnit/src/test/java/com/springbootjunit/repo/PersonRepoTest.java
+++ SpringBootJUnit/src/test/java/com/springbootjunit/repo/PersonRepoTest.java
@@ -1,6 +1,8 @@
 package com.springbootjunit.repo;
 
 import com.springbootjunit.entity.Person;
+import org.junit.jupiter.api.AfterEach;
+import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.boot.test.context.SpringBootTest;
@@ -15,11 +17,20 @@ class PersonRepoTest {
 
     @Test
     void isPersonExitsByIdTest() {
-        Person person = new Person(101, "Kumar",  "Bengaluru");
+        Person person = new Person(101, "Kumar", "Bengaluru");
         personRepo.save(person);
 
         Boolean actualResult = personRepo.isPersonExitsById(101);
         assertThat(actualResult).isTrue();
+    }
+    @BeforeEach
+    void setUp() {
+        System.out.println("Setting Up - This method call before testcase eg: @Test ");
+    }
 
+    @AfterEach
+    void tearDown() {
+        System.out.println("Tearing Down - This annotation execute after @Test and Remove everything ");
+        personRepo.deleteAll();
     }
 }
